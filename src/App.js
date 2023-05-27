33b7983f3e9919e3191f3b7b2e9f49e31bb54baa
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-05-27 16:50:57 +0600

    toast add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { toast, ToastContainer } from 'react-toastify';
 import './App.css';
 import Activities from './components/Activities/Activities';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const notify = () => {
+    toast.success('🧘 Meditation Completed!', {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
   return (
     <div>
-      <Activities></Activities>
+      <ToastContainer />
+      <Activities notify={notify}></Activities>
     </div>
   );
 }
